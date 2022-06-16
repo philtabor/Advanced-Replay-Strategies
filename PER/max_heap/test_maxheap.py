2c1fcc4275326225769ce8a303c4981dafe5cad3
philtabor <ptabor@gmail.com>
2022-06-16 09:56:14 -0400

    removed solution

--- PER/max_heap/test_maxheap.py
+++ PER/max_heap/test_maxheap.py
@@ -2,26 +2,11 @@ import numpy as np
 
 
 def max_heapify(array, i, N=None):
-    N = len(array) if N is None else N
-    left = 2 * i + 1
-    right = 2 * i + 2
-    largest = i
-
-    if left < N and array[left] > array[i]:
-        largest = left
-    if right < N and array[right] > array[largest]:
-        largest = right
-    if largest != i:
-        array[i], array[largest] = array[largest], array[i]
-        max_heapify(array, largest, N)
-    return array
+    pass
 
 
 def build_max_heap(array):
-    N = len(array)
-    for i in range(N//2, -1, -1):
-        array = max_heapify(array, i)
-    return array
+    pass
 
 
 if __name__ == '__main__':
